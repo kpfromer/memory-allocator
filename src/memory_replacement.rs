f679b8c235ae4a65661524eceb84bec71ade7d39
Kyle Pfromer <kpfromer2@gmail.com>
2021-04-16 12:06:26 -0600

    wip: add temp files

--- src/memory_replacement.rs
+++ src/memory_replacement.rs
@@ -1,17 +1,74 @@
-pub struct MemoryReplacement {
-    frame_size: usize,
-    frames: Vec<Vec<i32>>,
-    // deque: VecDeque<i32>,
+use anyhow::{anyhow, bail, Result};
 
-    // fn new(frames: usize, accesses: usize) -> Self;
-    // fn run(&mut self, accesses: &Vec<i32>) -> MemoryAccesses;
-    // fn gen_table(&self) -> Table;
+trait MemoryReplacement {
+    fn get_page_to_remove(&self) -> Option<i32>;
+    fn add(&mut self, access: i32) -> Result<()>;
+    fn replace(&mut self, old_value: i32, new_value: i32) -> Result<()>;
+    fn upsert(&mut self, value: i32) -> Result<()>;
+    // fn run(self) -> Result<>;
 }
 
-impl MemoryReplacement {
-    fn add() {}
-    fn replace() {}
-    fn upsert() {}
+// pub struct MemoryReplacement {
+//     frame_size: usize,
+//     frames: Vec<Vec<i32>>,
+//     position: usize, // deque: VecDeque<i32>,
 
-    fn run() -> MemoryAccesses {}
-}
+//                      // fn new(frames: usize, accesses: usize) -> Self;
+//                      // fn run(&mut self, accesses: &Vec<i32>) -> MemoryAccesses;
+//                      // fn gen_table(&self) -> Table;
+// }
+
+// impl MemoryReplacement {
+//     // fn get_page_to_remove(&self) -> Option<i32> {}
+
+//     fn get_page_to_remove(&self) -> Option<usize> {
+
+//     }
+
+//     fn add(&mut self, access: i32) -> Result<()> {
+//         self.frames[self.position].push(access);
+
+//         if self.frames[self.position].len() > self.frame_size {
+//             bail!("Invalid frame add");
+//         }
+
+//         Ok(())
+//     }
+
+//     // fn replace(&mut self, old_value: i32, new_value: i32) -> Result<()> {
+//     //     let old_pos = self.frames[self.position]
+//     //         .iter()
+//     //         .position(|&value| value == old_value)
+//     //         .ok_or(anyhow!(
+//     //             "Can not replace \"{}\" since it's value was not found in frame.",
+//     //             old_value
+//     //         ))?;
+
+//     //     self.frames[old_pos][old_pos] = new_value;
+
+//     //     Ok(())
+//     // }
+
+//     fn replace(&mut self, old_index: usize, new_value: i32) -> Result<()> {
+//         self.frames[self.position][old_index] = new_value;
+
+//         Ok(())
+//     }
+
+//     fn upsert(&mut self, value: i32) -> Result<()> {
+//         if !self.frames[self.position].contains(&value) {
+//             if self.frames[self.position].len() == self.frame_size {
+//                 // Replace something
+//                 let to_remove = self.get_page_to_remove().unwrap();
+//                 self.replace(to_remove, value);
+//             } else {
+//                 // We can just add it
+//                 self.frames[self.position].push(value);
+//             }
+//         }
+
+//         Ok(())
+//     }
+
+//     // fn run(self) -> MemoryAccesses {}
+// }
